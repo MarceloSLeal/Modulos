3eacf0666c5d7f677641136b98fb6ede912c7d57
MarceloSLeal <marcelosleal@outlook.com>
2023-05-04 03:18:36 -0300

    Novo módulo e um exemplo de export transitive, que expôes o módulo outro módulo que não está diretamente conectado é como um bypass, exemplo: uma classe no módulo A chamando uma classe no módulo B que tem acesso ao módulo C que usou um requires transitive, a classe no módulo A vai conseguir executar a classe no módulo C mesmo sem ter acesso direto

--- app-calculo/src/main/java/br/com/marcelo/calculo/Calculadora.java
+++ app-calculo/src/main/java/br/com/marcelo/calculo/Calculadora.java
@@ -1,12 +1,14 @@
 package br.com.marcelo.calculo;
 
 import br.com.marcelo.calculo.interno.OperacoesAritmeticas;
+import br.com.marcelo.logging.Logger;
 
 public class Calculadora {
 
     private OperacoesAritmeticas operacoesAritmeticas = new OperacoesAritmeticas();
 
     public double soma(double... nums) {
+        Logger.info("Somando");
         return operacoesAritmeticas.soma(nums);
     }
 }
